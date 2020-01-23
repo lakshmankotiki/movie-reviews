8fb827f0872a9b5f023e2be86d53bf2122acc87f
Lakshman Kotiki <kotiki.lakshman1996@gmail.com>
2020-01-23 16:58:37 +0530

    fetched data from movies api and dispalyed on body

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ var body = document.querySelector('body');
 var searchBtn = document.querySelectorAll('input')[0];
 var submitBtn = document.querySelectorAll('input')[1];
 
+var section = document.querySelector('section');
 var movieDisplay = document.querySelector(".lead");
+
+if (searchBtn.value === '') {
+    document.querySelector('.jumbotron-class').style.display = "none";
+}
 
 submitBtn.addEventListener('click', submitHandler);
 
 function submitHandler(e) {
+    document.querySelector('.jumbotron-class').style.display = "block";
     fetchResults(e);
 }
 
@@ -22,8 +28,34 @@ function fetchResults(e) {
         fetch(url).then(function(response) {
             return response.json();
         }).then(function(data) {
-            var movieDetails = JSON.stringify(data);
 
+            displayMovies(data);
         });
     }
+}
+
+
+function displayMovies(data) {
+    // var movieDetails = data;
+    // for(var i=0;i<movieDetails.length;i++) {
+
+    // }
+    // var movieName = document.querySelector('.display-4');
+    // var headLine = document.createElement('h3');
+    // var some = document.querySelector('.container-class');
+    // var summary = document.querySelector('.summary');
+    // var releasedDate = document.createElement('h4');
+    // var artcileLink = document.createElement('a');
+    // var movieImage = document.createElement('img');
+    // movieName.innerText = movieDetails.results[0].display_title;
+    // headLine.innerText = movieDetails.results[0].headline;
+    // summary.innerText = movieDetails.results[0].summary_short;
+    // artcileLink.href = movieDetails.results[0].link.url;
+    // artcileLink.innerText = movieDetails.results[0].link.suggested_link_text;
+    // movieImage.src = movieDetails.results[0].multimedia.src;
+    // some.appendChild(headLine);
+    // // body.appendChild(summary);
+    // body.appendChild(artcileLink);
+    // body.appendChild(movieImage);
+    // window.console.log("movieDetails", movieDetails)
 }
